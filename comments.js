8da0c7f10b5155a53a093a73e19b45e73cda7920
kpoluru2606 <56950225+kpoluru2606@users.noreply.github.com>
2024-10-07 10:43:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,23 +1,18 @@
 // Create web server     
-// 1. Create a web server
-// 2. Create a route for /comments
-// 3. Read the comments.json file
-// 4. Display the comments in the browser
-// 5. Add a form to add comments
+// 1. Import express
+// 2. Create an instance of express
+// 3. Create a route for GET /comments
+// 4. Send a response with a list of comments
+// 5. Start the server on port 3000
+// 6. Test the server with Postman
 
-// 1. Create a web server
 const express = require('express');
 const app = express();
-const fs = require('fs');
 
-// 2. Create a route for /comments
 app.get('/comments', (req, res) => {
-    // 3. Read the comments.json file
-    fs.readFile('comments.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Could not read comments file');
-            return;
-        }
-        const comments = JSON.parse(data);
-        /
-    
+    res.send('This is a list of comments');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
