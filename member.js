421851f9ad9c0485fa5ea32a95bf90dabdb130c4
kpoluru2606 <56950225+kpoluru2606@users.noreply.github.com>
2024-10-07 09:32:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember(){
+    let name = 'John';
+    let age = 25;
+    let skills = ['HTML', 'CSS', 'JS'];
+    console.log(`Name is ${name}, age is ${age}, skills: ${skills}`);
+}
